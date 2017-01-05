29d5953534547c8f9f38fcfe99314d8795953b5b
Frederik <frederik.smekens@hotmail.be>
2017-01-05 12:49:50 +0100

    gebruiker naar user verandert

--- services/GebruikerService.php
+++ services/GebruikerService.php
@@ -2,20 +2,20 @@
 require_once 'data/GebruikerDAO.php';
 class GebruikerService 
 {
-    public function registreerGebruiker($email, $paswoord)
+    public function registreerUser($email, $paswoord)
     {
         //verwijst naar functie registreerGebruiker in daogebruiker.php voor registreren van gebruiker
-        $gebruiker = new $GebruikerDAO();
-        $gebruiker = $GebruikerDAO->registreerGebruiker($email, $paswoord);
-        return Gebruiker;
+        $user = new UserDAO();
+        $user = $UserDAO->registreerGebruiker($email, $paswoord);
+        return $user;
     }
     
-    public function toonAlleGebruikers()
+    public function toonAlleUsers()
     {
-        $GebruikerDAO = new $GebruikerDAO();
-        $alleGebruikers = $GebruikerDAO->getAlleGebruikers();
+        $UserDAO = new $UserDAO();
+        $alleUsers = $UserDAO->getAlleUSers();
      
-        return $alleGebruikers;
+        return $alleUsers;
         
     }
 }
