6258ab0f060aa7b8dc55727f360313dee2610bf4
Leen <leenverbiest1@gmail.com>
2017-01-09 09:42:39 +0100

    data folder met DBCONFIG.php aangemaakt en bestandje dbGegevensToevoegen.php verwijderd

---
+++ data/DBCONFIG.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyber10
+ * Date: 9/01/2017
+ * Time: 9:38
+ */
+//data/DBCONFIG.php
+class DBCONFIG
+{
+
+    public static $DB_CONNSTRING = "mysql:host=192.168.0.21;dbname=Datingsite;charset=utf8";
+    public static $DB_USERNAME = "root";
+    public static $DB_PASSWORD = "vdab";
+
+}
