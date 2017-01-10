ef5254f9302a28465c9a237634c568fa541217be
Frederik <frederik.smekens@hotmail.be>
2017-01-10 08:51:54 +0100

    twig toegevoegd en gelinkt

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
 require_once 'services/GebruikerService.php';
-$gebService = new GebruikerService();
-$lijst = $gebService -> toonAlleUsers();
-print_r($lijst);
+
+require_once 'library/vendor/twig/autoloader.php';
+Twig_Autoloader::register();
+
+//vertel Twig in welke map de templates zitten
+$loader = new Twig_Loader_Filesystem('presentation');
+//laad nieuwe Twig Environment vanuit die map
+$twig = new Twig_Environment($loader);
+
+
+    $view = $twig->render('index.twig');
+
+
+//toon de pagina
+print($view);
--- services/GebruikerService.php
+++ services/GebruikerService.php
@@ -2,13 +2,6 @@
 require_once 'data/GebruikerDAO.php';
 class GebruikerService 
 {
-    public function createUser($id,$email, $paswoord)                           //verwijst naar functie createUser in daogebruiker.php voor aanmaken gebruiker
-    {        
-        $user = new UserDAO();
-        $user = $UserDAO->createUser($id,$email, $paswoord);
-        return $user;
-    }
-    
     public function toonAlleUsers()                                             
     {
         $GebruikerDAO = new GebruikerDAO();
@@ -16,11 +9,17 @@ class GebruikerService
         return $alleUsers;        
     }
     
+    public function createUser($id,$email, $paswoord)                           //verwijst naar functie createUser in daogebruiker.php voor aanmaken gebruiker
+    {        
+        $user = new UserDAO();
+        $user = $UserDAO->createUser($id,$email, $paswoord);
+        return $user;
+    }    
+    
     public function updateUser($id,$email,$paswoord)
     {
         $userDAO = new $UserDAO();
         $user = $UserDAO -> updateUser($id,$email,$paswoord);
-        return $user;
+        return $user;        
     }
 }
-
