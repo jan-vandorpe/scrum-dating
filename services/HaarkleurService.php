a55211d4409aca1e89e944ebc0a00e813a554b79
Leen <leenverbiest1@gmail.com>
2017-01-17 12:32:48 +0100

    Service haarkleur verder afgewerkt

---
+++ services/HaarkleurService.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyber10
+ * Date: 17/01/2017
+ * Time: 8:47
+ */
+//Services/HaarkleurService.php
+require_once ('data/HaarkleurDAO.php');
+
+class HaarkleurService {
+//READ
+    public function toonAlleHaarkleuren()
+    {
+        $haarkleurDAO=new HaarkleurDAO();
+        $alleHaarkleuren=$haarkleurDAO->getAlleHaarkleuren();
+        return $alleHaarkleuren;
+    }
+
+    //CREATE
+    public function createHaarkleur($haarkleur)
+    {
+        $haarkleurDAO=new HaarkleurDAO();
+        $alleHaarkleuren=$haarkleurDAO->createHaarkleur($haarkleur);
+        return $alleHaarkleuren;
+    }
+
+    //UPDATE
+    public function updateHaarkleur($haarkleur)
+    {
+        $haarkleurDAO=new HaarkleurDAO();
+        $alleHaarkleuren=$haarkleurDAO->updateHaarkleur($haarkleur);
+        return $alleHaarkleuren;
+    }
+
+    //DELETE
+    public function deleteHaarkleur($haarkleurId)
+    {
+        $haarkleurDAO=new HaarkleurDAO();
+        $alleHaarkleuren=$haarkleurDAO->delete($haarkleurId);
+        return $alleHaarkleuren;
+    }
+
+}
