b85513f4d37649a5261e379c934d133c2778f016
JustinReynaert <Justin.Reynaert@gmail.com>
2017-01-24 11:00:17 +0100

    validatie + QoL

--- js/voorkeuren.js
+++ js/voorkeuren.js
@@ -1,4 +1,65 @@
-$(function(){
-	if(jQuery().validate) {console.log("validate geladen"); }
-	else {console.log("Validate niet geladen");}
-});
+$(function() {
+
+	form = $("#voorkeurForm");
+
+	$(".default").attr('name', 'chzn-search');
+
+
+	form.submit(function (e) {
+		e.preventDefault()
+	});
+
+
+	form.validate({
+		debug: true,
+
+		rules: {
+
+			lengte: {
+				digits: true,
+				minlength: 2,
+				maxlength: 3,
+				max: 272,
+				min: 54
+
+			}
+		/*	,
+
+			geboortedatum: {
+				dateISO: true
+			}*/
+
+
+		},
+
+		messages: {
+
+			lengte: {
+				digits: "Een lengte bestaat enkel uit getallen",
+				minlength: "Het getal moet minstens uit 2 getallen bestaan",
+				maxlength: "Het getal mag maximum uit 3 getallen bestaan",
+				max: "Lengte kan niet hoger dan 272cm zijn",
+				min: "Lengte kan niet kleiner dan 54cm zijn"
+
+			}
+			/*,
+
+			geboortedatum:{
+				dateISO: "datum moet in het YYYY-MM-DD formaat"
+			}*/
+
+
+		},
+
+		submitHandler: function (form) {
+			form.submit();
+
+		}
+
+
+	});
+
+
+});// einde window onload
+
+	
