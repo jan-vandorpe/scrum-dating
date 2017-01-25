8bc0b4b4d3ea9ea12164a7e3476534ad91ab5b78
Frederik <frederik.smekens@hotmail.be>
2017-01-25 10:10:49 +0100

    niet kunnen zien als niet ingelogd

--- wijzigwachtwoord.php
+++ wijzigwachtwoord.php
@@ -1,36 +1,39 @@
 <?php
-session_start();
-require_once 'services/GebruikerService.php';
-require_once 'entities/Gebruiker.php';
 
-if (isset($_POST['wwWijzigen']))
-{    
- 
-    $gebruikerId = $_SESSION['gebruikerId'];
-    
-    $oudWachtwoord = $_POST['oudWW'];
-    $nieuwWachtwoord = $_POST['nieuwWW'];
-    $nieuwWachtwoord2 = $_POST['nieuwWW2'];
-    
-    $gebService = new GebruikerService();
-    $gebruiker = $gebService -> getById($gebruikerId);
-    
-  
-    $email= $gebruiker->getEmail();
-    
-    $gebSvc = new GebruikerService();
-    $loginCheck = $gebSvc->checkLogin($email, $oudWachtwoord);
-   
-    if($loginCheck == true)
-    {
-  
-        if($nieuwWachtwoord == $nieuwWachtwoord2)
-        {
-            
+session_start();
+if (isset($_SESSION["gebruikerId"])) {
+    require_once 'services/GebruikerService.php';
+    require_once 'entities/Gebruiker.php';
+
+    if (isset($_POST['wwWijzigen'])) {
+
+        $gebruikerId = $_SESSION['gebruikerId'];
+
+        $oudWachtwoord = $_POST['oudWW'];
+        $nieuwWachtwoord = $_POST['nieuwWW'];
+        $nieuwWachtwoord2 = $_POST['nieuwWW2'];
+
+        $gebService = new GebruikerService();
+        $gebruiker = $gebService->getById($gebruikerId);
+
+
+        $email = $gebruiker->getEmail();
+
+        $gebSvc = new GebruikerService();
+        $loginCheck = $gebSvc->checkLogin($email, $oudWachtwoord);
+
+        if ($loginCheck == true) {
+
+            if ($nieuwWachtwoord == $nieuwWachtwoord2) {
+
                 $gebSvc = new GebruikerService();
                 $gebSvc->updateWachtwoord($gebruikerId, $nieuwWachtwoord);
+            }
         }
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
-      header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
-    
+else {
+    $location = 'index.php';
+    header('Location: ' . $location);
+}
