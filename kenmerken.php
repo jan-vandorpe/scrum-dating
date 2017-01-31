b31b21413e1db83a2d4f4465b967495b16713a94
JustinReynaert <Justin.Reynaert@gmail.com>
2017-01-31 09:21:22 +0100

    Fileupload size naar 5MB

--- kenmerken.php
+++ kenmerken.php
@@ -60,7 +60,7 @@ if (isset($_POST['updateKenmerken']))
         }
 
 // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
+    if ($_FILES["fileToUpload"]["size"] > 5242880) {
       //  echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
